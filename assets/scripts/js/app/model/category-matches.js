c00189f1726a77b4bc3e34be38c1319ef8ac5416
Peter Seliger <peter.seliger@xing.com>
2014-06-08 13:15:29 +0200

    final version before merge with d3.js by Aurelius.

--- assets/scripts/js/app/model/category-matches.js
+++ assets/scripts/js/app/model/category-matches.js
@@ -20,29 +20,100 @@
   //},
 
 
+    noop = function () {},
+
+
+    categoryFilterMap = {
+
+      "creative-minds": function (matchData) {
+        return (
+              (matchData.traitDataMap.openness      >= 70)
+          &&  (matchData.traitDataMap.neuroticism   >= 70)
+        );
+      },
+      "best-buddies": function (matchData) {
+        return (
+              (matchData.traitDataMap.openness      >= 70)
+          &&  (matchData.traitDataMap.agreeableness >= 70)
+          &&  (matchData.traitDataMap.neuroticism   <= 30)
+        );
+      },
+      "workhorses": function (matchData) {
+        return (
+              (matchData.traitDataMap.consciousness >= 70)
+          &&  (matchData.traitDataMap.extraversion  >= 50)
+          &&  (matchData.traitDataMap.agreeableness >= 50)
+        );
+      },
+      "happy-spouses": function (matchData) {
+        return (
+              (matchData.traitDataMap.openness      >= 70)
+          &&  (matchData.traitDataMap.agreeableness >= 70)
+          &&  (matchData.traitDataMap.neuroticism   <= 30)
+        );
+      },
+      "party-kings": function (matchData) {
+        return (
+              (matchData.traitDataMap.openness      >= 50)
+          &&  (matchData.traitDataMap.consciousness <= 30)
+          &&  (matchData.traitDataMap.extraversion  >= 70)
+        );
+      }/*,
+      "special_precondition": function (matchData) {
+        return matchData.traitDataList.some(function (traitData) {
+
+          return (traitData.value >= 70);
+        });
+      }*/
+    },
+    filterModelByCategory = function (matchesModel, categoryType) {
+      var filteredModel = [];
+      if ((categoryType == "special-cases")/* && matchesModel.some(categoryFilterMap["special_precondition"])*/) {
+
+      } else {
+        filteredModel = matchesModel.filter(categoryFilterMap[categoryType] || noop);
+      }
+      return filteredModel;
+    },
+
+
     mapMatchData = function (obj) {
       return {
+
+
+    // used by the view
 
         avatarSrc : toString(obj.picture),
         accountId : toString(obj.tHand),
         realName  : toString(obj.name),
 
         traitDataList : [{
-          name  : "Consciousness",
-          value : toNumber(obj.traits.consc)
-        }, {
           name  : "Openness",
           value : toNumber(obj.traits.openn)
         }, {
-          name  : "Neurotic",
-          value : toNumber(obj.traits.neuro)
-        }, {
-          name  : "Agreeability",
-          value : toNumber(obj.traits.agree)
+          name  : "Consciousness",
+          value : toNumber(obj.traits.consc)
         }, {
           name  : "Extraversion",
           value : toNumber(obj.traits.extra)
-        }]
+        }, {
+          name  : "Agreeableness",
+          value : toNumber(obj.traits.agree)
+        }, {
+          name  : "Neuroticism",
+          value : toNumber(obj.traits.neuro)
+        }],
+
+
+    // used by the model itself in order to support easy filtering
+
+        traitDataMap : {
+          "openness"      : toNumber(obj.traits.openn),
+          "consciousness" : toNumber(obj.traits.consc),
+          "extraversion"  : toNumber(obj.traits.extra),
+          "agreeableness" : toNumber(obj.traits.agree),
+          "neuroticism"   : toNumber(obj.traits.neuro)
+        }
       };
     },
     reduceMatchDataList = function (list) {
@@ -60,7 +131,9 @@
 
   //copyModel   : createCopy, // need to figure out why this didn't really work out - prototypal copy on write clones are faster.
     copyModel   : reduceMatchDataList,
-    createModel : reduceMatchDataList
+    createModel : reduceMatchDataList,
+
+    filterModelByCategory: filterModelByCategory
   };
 
 
